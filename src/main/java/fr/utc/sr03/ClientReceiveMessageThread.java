42b1cda64ecb9a2a10861a842bcf54df3eaf1996
CRedSk <132748251+CRedSk@users.noreply.github.com>
2024-03-25 19:00:22 +0100

    Update ClientReceiveMessageThread.java

--- src/main/java/fr/utc/sr03/ClientReceiveMessageThread.java
+++ src/main/java/fr/utc/sr03/ClientReceiveMessageThread.java
@@ -2,6 +2,7 @@ package fr.utc.sr03;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 
 //un thread qui va intercepter les messages envoyés par le serveur puis les afficher dans la console.
 public class ClientReceiveMessageThread extends Thread{
@@ -18,24 +19,48 @@ public class ClientReceiveMessageThread extends Thread{
             ex.printStackTrace();
         }
     }
-    private boolean exit=false;
+    boolean exit=false;
+//    int bytesRead = input.read();
+//                if(bytesRead==-1){
+//        exit=true;
+//        return;
+//    }
+    String message;
+
     public void run(){
         try {
             while(!exit){
-                String message = dataInputStream.readUTF();//attendre les messages envoyés par serveur
-                //System.out.println(message);
-                if (message.contentEquals("exit")){
+                if(!commSocket.isConnected()){
                     input.close();
                     dataInputStream.close();
-                    exit=true;
+                    exit = true;
                     commSocket.close();
-                }else{
-                    System.out.println(message);
+                }else {
+                    try {
+                        message = dataInputStream.readUTF();//attendre les messages envoyés par serveur
+                    }catch(SocketException ex){
+                        commSocket.close();
+                        dataInputStream.close();
+                        message="";
+                        System.out.println("Exit avec succes");
+
+                        exit=true;
+                    }
+                    if (message.contentEquals("exit")) {
+                        input.close();
+                        dataInputStream.close();
+                        exit = true;
+                        commSocket.close();
+                    } else {
+                        if(commSocket.isConnected()) {
+                            System.out.println(message);
+                        }
+                    }
                 }
             }
 
         }catch (IOException ex) {
-            ex.printStackTrace();
+               ex.printStackTrace();
         }
     }
 
